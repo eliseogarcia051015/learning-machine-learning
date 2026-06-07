8f000a2d033cad0942418e02c22e64afbf04fc95
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-06-07 04:27:11 -0500

    libraries added + csv data gathered

--- HousingPredictor.py
+++ HousingPredictor.py
@@ -1,8 +1,13 @@
 import kagglehub
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from importlib.metadata import version
 
-#KAGGLE API
-path = kagglehub.dataset_download("camnugent/california-housing-prices")
+#KAGGLE API KEY
+folder_path = kagglehub.dataset_download("camnugent/california-housing-prices")
+#print("Path to dataset files:", path)
 
-print(version('kagglehub'))
-print("Path to dataset files:", path)
+data = pd.read_csv(f"{folder_path}/housing.csv")
+print(data)
