20bc8037519b98da3d0270b9dbf2746386ac3ad7
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-06-07 00:53:07 -0500

    Added Kaggle API key to retrieve data

--- HousingPredictor.py
+++ HousingPredictor.py
@@ -1,1 +1,8 @@
-#
+import kagglehub
+from importlib.metadata import version
+
+#KAGGLE API
+path = kagglehub.dataset_download("camnugent/california-housing-prices")
+
+print(version('kagglehub'))
+print("Path to dataset files:", path)
